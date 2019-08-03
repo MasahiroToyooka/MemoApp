125dbce88afa9696cd32d0c654b254a50fc291d9
MasahiroToyooka <ehara2481@gmail.com>
2019-08-03 14:22:22 +0900

    initial commit

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate{
 
+    /// メモ
+    @IBOutlet weak var textView: UITextView!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // textViewがUITextViewDelegateを使えるようにします。
+        textView.delegate = self
+        
+        //Appdelegateを参照するための定数
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        // AppDelegateに定義したlastTextを参照し、textViewに格納する。
+        textView.text = appDelegate.lastText
     }
 
-
+    //テキストが変更されたら呼ばれる
+    func textViewDidChange(_ textView: UITextView) {
+        // AppDelegateを呼び出して変数に格納します。
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        // AppDelegateに記述した"lastText"に入力されている内容を格納します。
+        appDelegate?.lastText = textView.text
+    }
 }
+
+
+
 
